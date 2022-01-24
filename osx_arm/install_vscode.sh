07f1fbd26c462be6e0a41474b0a29ed9410382a9
despiegk <kristof@incubaid.com>
2022-01-24 08:30:52 +0400

    init

--- osx_arm/install_vscode.sh
+++ osx_arm/install_vscode.sh
@@ -4,11 +4,14 @@ pushd /tmp
 rm -f vscode.zip
 curl -L 'https://code.visualstudio.com/sha/download?build=stable&os=darwin-arm64' -o vscode.zip
 
+rm -rf Visual\ Studio\ Code.app
+rm -rf /Applications/Visual\ Studio\ Code.app
+
 unzip vscode.zip
 
-rsync -rav Visual\ Studio\ Code.app\ /Applications/Visual\ Studio\ Code.app\
+rsync -rav Visual\ Studio\ Code.app/ /Applications/Visual\ Studio\ Code.app/
 
-rm -rf /Applications/Visual\ Studio\ Code.app
+
 
 popd
 
