10ad09d93ac2014e34a7c99628464082c5e8c3c6
despiegk <kristof@incubaid.com>
2022-01-24 08:00:20 +0400

    init

--- osx_arm/install.sh
+++ osx_arm/install.sh
@@ -1,1 +1,5 @@
-cp libssl.1.1.dylib /usr/local/lib/libssl.1.1.dylib
+sudo mkdir -p /usr/local/lib
+sudo cp libssl.1.1.dylib /usr/local/lib/libssl.1.1.dylib
+sudo cp libcrypto.1.1.dylib /usr/local/lib/libcrypto.1.1.dylib
+sudo chown `WHOAMI`:staff  /usr/local/lib/libssl.1.1.dylib
+sudo chown `WHOAMI`:staff  /usr/local/lib/libcrypto.1.1.dylib
