009ec9b22d72369d8707a62112a92670b5886b05
despiegk <kristof@incubaid.com>
2022-01-24 08:54:25 +0400

    init

--- osx_arm/install_vscode.sh
+++ osx_arm/install_vscode.sh
@@ -11,11 +11,34 @@ unzip vscode.zip
 
 rsync -rav Visual\ Studio\ Code.app/ /Applications/Visual\ Studio\ Code.app/
 
+rm -f vscode.zip
+rm -rf Visual\ Studio\ Code.app
 
 
 popd
 
-cat << EOF >> ~/.bash_profile
+#have to do this workaround on osx, can't do infile -i
+sed '/Visual Studio/d' ~/.zprofile > /tmp/1 && mv /tmp/1 ~/.zprofile
+# sed '/Visual Studio/d' ~/.bash_profile > /tmp/1 && mv /tmp/1 ~/.bash_profile
+
+cat << EOF >> ~/.zprofile
 # Add Visual Studio Code (code)
 export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 EOF
+
+# cat << EOF >> ~/.bash_profile
+# # Add Visual Studio Code (code)
+# export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+# EOF
+
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting
+code --install-extension mushan.vscode-paste-image
+code --install-extension vlanguage.vscode-vlang
+code --install-extension ms-vscode-remote.remote-ssh
+
+
+echo "VSCODE INSTALLED SUCCESFULLY"
